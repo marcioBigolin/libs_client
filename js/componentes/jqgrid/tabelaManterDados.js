51e24a6ecaa1a78301265ec676857bb4974d6a40
Marcio Bigolin <marcio.bigolin@ucs.br>
2014-02-28 18:43:09 -0300

    Tabela melhorada

--- js/componentes/jqgrid/tabelaManterDados.js
+++ js/componentes/jqgrid/tabelaManterDados.js
@@ -1,27 +1,27 @@
-function editarDado(){
-  var id = $("#tabelaDadosManterDados").jqGrid('getGridParam','selrow');
+function editarDado() {
+    var id = $("#tabelaDadosManterDados").jqGrid('getGridParam', 'selrow');
     //alert(gr);
-    if( id != null ) {
+    if (id != null) {
         //$("#tabelaDados_manterDados").jqGrid('delGridRow',gr,{reloadAfterSubmit:false}); 
-         location.href = tabelaManterDados.acaoEditarDado+ '?id=' + id;
+        location.href = tabelaManterDados.acaoEditarDado + '?id=' + id;
     } else {
         alert("Selecione um dado!");
     }
 
 }
 
-function adicionarDado(){
+function adicionarDado() {
     location.href = tabelaManterDados.acaoAdicionarDado;
 }
 
 
-function deletarDado(){
-    var id = $("#tabelaDadosManterDados").jqGrid('getGridParam','selrow'); 
-    if (id) { 
-        if(window.confirm("Tem certeza que deseja apagar este dado? \n Essa ação é irreversível.")){            
-            location.href = tabelaManterDados.acaoDeletarDado+'?id='+id;
+function deletarDado() {
+    var id = $("#tabelaDadosManterDados").jqGrid('getGridParam', 'selrow');
+    if (id) {
+        if (window.confirm("Tem certeza que deseja apagar este dado? \n Essa ação é irreversível.")) {
+            location.href = tabelaManterDados.acaoDeletarDado + '?id=' + id;
         }
-    } else { 
+    } else {
         alert("Por favor selecione um dado!");
     }
 }
@@ -41,91 +41,91 @@ $(document).ready(function()
         useRp: true,
         singleSelect: tabelaManterDados.selecaoSimples,
         pager: tabelaPaginacao,
-        rowNum:40,
-        rowList:[40,80,60],
+        rowNum: 40,
+        rowList: [40, 80, 200],
         editUrl: tabelaManterDados.dados,
         width: largura,
-        height: 250
-        
-   
-    };
-     $("#tabelaDadosManterDados").jqGrid(objeto);
-       $("#tabelaDadosManterDados").jqGrid('gridResize',{
-            minWidth:500,
-            maxWidth:largura, 
-            minHeight:80, 
-            maxHeight:800
-        });
+        height: tabelaManterDados.altura
 
-   
-    $('#tabelaDadosManterDados').navGrid(tabelaPaginacao, {  
-        search: true, 
-        edit: false, 
-        add: false, 
+
+    };
+    $("#tabelaDadosManterDados").jqGrid(objeto);
+    $("#tabelaDadosManterDados").jqGrid('gridResize', {
+        minWidth: 500,
+        maxWidth: largura,
+        minHeight: 80,
+        maxHeight: 1000
+    });
+
+
+    $('#tabelaDadosManterDados').navGrid(tabelaPaginacao, {
+        search: true,
+        edit: false,
+        add: false,
         del: false
-    }, 
-{}, {}, {}, 
-{
-        closeOnEscape: true, 
-        multipleSearch: true, 
-        closeAfterSearch: true
-    }
+    },
+    {}, {}, {},
+            {
+                closeOnEscape: true,
+                multipleSearch: true,
+                closeAfterSearch: true
+            }
     );
 
-$('#tabelaDadosManterDados').jqGrid('filterToolbar',{stringResult: true,searchOnEnter : false});
+    $('#tabelaDadosManterDados').jqGrid('filterToolbar', {stringResult: true, searchOnEnter: false});
 
-    $('#tabelaDadosManterDados').navSeparatorAdd(tabelaPaginacao, 
-    {
-        sepclass: 'ui-separator', 
-        sepcontent: ''
-    }); 
-    
-    //Botão deletar dados  
-    if(tabelaManterDados.botaoDeletar){
-        $('#tabelaDadosManterDados').jqGrid('navButtonAdd', 
-            tabelaPaginacao, { 
-                caption: "", 
-                buttonicon: "ui-icon-trash", 
-                onClickButton: deletarDado,
-                position: "last", 
-                title: "Deletar dado", 
-                cursor: "hand"
-            });                    
-    }
- 
-    //Botão editar dados                        
-    if(tabelaManterDados.botaoEditar){
-        $('#tabelaDadosManterDados').jqGrid('navButtonAdd', 
-            tabelaPaginacao, { 
-                caption: "", 
-                buttonicon: "ui-icon-pencil", 
-                onClickButton: editarDado,
-                position: "last", 
-                title: "Editar dado", 
-                cursor: "hand"
-            });                    
-                    
-    }
- 
-    //Botão de adicionar Dados   
-    if(tabelaManterDados.botaoAdicionar){   
-        $('#tabelaDadosManterDados').jqGrid('navButtonAdd', 
-            tabelaPaginacao, { 
-                caption: "", 
-                buttonicon: "ui-icon-plusthick", 
-                onClickButton: adicionarDado,
-                position: "last", 
-                title: "Adicionar dado", 
-                cursor: "hand"
+    $('#tabelaDadosManterDados').navSeparatorAdd(tabelaPaginacao,
+            {
+                sepclass: 'ui-separator',
+                sepcontent: ''
             });
+
+    //Botão deletar dados  
+    if (tabelaManterDados.botaoDeletar) {
+        $('#tabelaDadosManterDados').jqGrid('navButtonAdd',
+                tabelaPaginacao, {
+                    caption: "",
+                    buttonicon: "ui-icon-trash",
+                    onClickButton: deletarDado,
+                    position: "last",
+                    title: "Deletar dado",
+                    cursor: "hand"
+                });
     }
 
-    $('#tabelaDadosManterDados').navSeparatorAdd(tabelaPaginacao, 
-    {
-        sepclass: 'ui-separator', 
-        sepcontent: ''
-    }); 
-    
+    //Botão editar dados                        
+    if (tabelaManterDados.botaoEditar) {
+        $('#tabelaDadosManterDados').jqGrid('navButtonAdd',
+                tabelaPaginacao, {
+                    caption: "",
+                    buttonicon: "ui-icon-pencil",
+                    onClickButton: editarDado,
+                    position: "last",
+                    title: "Editar dado",
+                    cursor: "hand"
+                });
 
-                                   
+    }
+
+    //Botão de adicionar Dados   
+    if (tabelaManterDados.botaoAdicionar) {
+        $('#tabelaDadosManterDados').jqGrid('navButtonAdd',
+                tabelaPaginacao, {
+                    caption: "",
+                    buttonicon: "ui-icon-plusthick",
+                    onClickButton: adicionarDado,
+                    position: "last",
+                    title: "Adicionar dado",
+                    cursor: "hand"
+                });
+    }
+
+    $('#tabelaDadosManterDados').navSeparatorAdd(tabelaPaginacao,
+            {
+                sepclass: 'ui-separator',
+                sepcontent: ''
+            });
+
+
+
 });
